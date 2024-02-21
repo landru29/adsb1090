7887c9019104192857958b55a22a58a9a88baa20
Cyrille Meichel <cyrille.meichel@gmail.com>
2024-02-21 18:53:19 +0100

    fix build

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+set -e
 
 for arg in "$@"
 do
     case $arg in
         "build-driver" )
             mkdir -p /project/c && cd /project/c
-            git clone https://github.com/osmocom/rtl-sdr.git
+            git clone --depth 1 --branch v2.0.1 https://github.com/osmocom/rtl-sdr.git
             cd rtl-sdr
             dpkg-buildpackage -us -uc
             cp /project/c/*.deb /deb/.
@@ -28,11 +29,11 @@ do
                 "aarch64" )
                     ARCH=arm64
                     ;;
+                "armv7l" )
+                    ARCH=armhf
+                    ;;
                 "armv6l" )
                     ARCH=armv6l
-                    ;;
-                "armv7l" )
-                    ARCH=armv7l
                     ;;
             esac
 
@@ -43,8 +44,19 @@ do
 
             cat /adsb1090-${ARCH}/DEBIAN/control | sed -e "s/amd64/${ARCH}/g">/tmp/control
             cp /tmp/control /adsb1090-${ARCH}/DEBIAN/control
-            
+
+            export CGO_ENABLED=1
+            export GOPROXY=direct
+
+            echo "== Getting dependencies =="
+
+            go mod download
+
+            echo "== Building =="
+
             go build -o /adsb1090-${ARCH}/usr/local/bin/adsb1090 -buildvcs=false ./cmd/adsb1090/...
+
+            echo "== Packaging =="
 
             dpkg-deb --build --root-owner-group /adsb1090-${ARCH}
             cp /adsb1090-${ARCH}.deb /deb/.
